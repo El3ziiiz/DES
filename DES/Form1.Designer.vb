4cf15cc540964c611b8abd207a6d0d4de59902b3
abdullah <abdullah@EL3ZIIIZVM>
2026-02-23 11:31:14 -0800

    Add project files.

---
+++ DES/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtBox = New TextBox()
+        bttn = New Button()
+        SuspendLayout()
+        ' 
+        ' txtBox
+        ' 
+        txtBox.Location = New Point(322, 165)
+        txtBox.Name = "txtBox"
+        txtBox.Size = New Size(205, 23)
+        txtBox.TabIndex = 0
+        txtBox.Text = "string"
+        ' 
+        ' bttn
+        ' 
+        bttn.Location = New Point(345, 224)
+        bttn.Name = "bttn"
+        bttn.Size = New Size(148, 78)
+        bttn.TabIndex = 1
+        bttn.Text = "click"
+        bttn.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(881, 501)
+        Controls.Add(bttn)
+        Controls.Add(txtBox)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtBox As TextBox
+    Friend WithEvents bttn As Button
+
+End Class
---
+++ DES/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub bttn_Click(sender As Object, e As EventArgs) Handles bttn.Click
+        Dim d As String = txtBox.Text
+        MsgBox(cut(d))
+    End Sub
+    Function cut(ByVal str1 As String)
+        Dim res As String = ""
+        For Each ch As Char In str1
+            res &= (Asc(ch) - 97).ToString() & ","
+        Next
+        Return res
+    End Function
+End Class
