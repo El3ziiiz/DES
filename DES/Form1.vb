8e46aae3ef357141c7c0f628f6e6f0b47d3cfd25
El3ziiiz . <anm2006trz@gmail.com>
2026-02-27 04:06:08 -0800

    added a number to binary function

--- DES/Form1.vb
+++ DES/Form1.vb
@@ -1,12 +1,20 @@
 ﻿Public Class Form1
     Private Sub bttn_Click(sender As Object, e As EventArgs) Handles bttn.Click
-        Dim d As String = txtBox.Text
-        MsgBox(cut(d))
+        Dim inputText As String = txtBox.Text.Trim()
+        If IsNumeric(inputText) Then
+            Dim numberValue As Integer = CInt(inputText)
+            Dim binaryValue As String = Convert.ToString(numberValue, 2).PadLeft(8, "0"c)
+            MsgBox(binaryValue)
+        Else
+            MsgBox(ConvertToBinary(inputText))
+        End If
     End Sub
-    Function cut(ByVal str1 As String)
+    Function ConvertToBinary(ByVal str1 As String) As String
         Dim res As String = ""
         For Each ch As Char In str1
-            res &= (Asc(ch) - 97).ToString() & ","
+            Dim asciiValue As Integer = Asc(ch)
+            Dim binaryValue As String = Convert.ToString(asciiValue, 2).PadLeft(8, "0"c)
+            res &= binaryValue & " "
         Next
         Return res
     End Function
