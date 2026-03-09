0989f8249cd1e8999c091e26f61f83d1e0761891
El3ziiiz . <anm2006trz@gmail.com>
2026-03-09 12:49:04 -0700

    adding initial permutation and spliting to left and right, deleted the string handiling

--- DES/Form1.vb
+++ DES/Form1.vb
@@ -1,21 +1,32 @@
 ﻿Public Class Form1
     Private Sub bttn_Click(sender As Object, e As EventArgs) Handles bttn.Click
         Dim inputText As String = txtBox.Text.Trim()
-        If IsNumeric(inputText) Then
-            Dim numberValue As Integer = CInt(inputText)
-            Dim binaryValue As String = Convert.ToString(numberValue, 2).PadLeft(8, "0"c)
-            MsgBox(binaryValue)
-        Else
-            MsgBox(ConvertToBinary(inputText))
+        If Not IsNumeric(inputText) Then
+            MsgBox("Please enter a number.")
+            Return
         End If
+
+        Dim numberValue As Integer = CInt(inputText)
+        If numberValue < 0 Or numberValue > 255 Then
+            MsgBox("Please enter a number between 0 and 255.")
+            Return
+        End If
+
+        Dim binaryValue As String = Convert.ToString(numberValue, 2).PadLeft(8, "0"c)
+        Dim shuffled As String = InitialPermutation(binaryValue)
+        Dim leftHalf As String = shuffled.Substring(0, 4)
+        Dim rightHalf As String = shuffled.Substring(4, 4)
+
+        MsgBox("Original 8-bit: " & binaryValue & vbCrLf &
+               "After IP:" & " (L): " & leftHalf & " (R): " & rightHalf)
     End Sub
-    Function ConvertToBinary(ByVal str1 As String) As String
-        Dim res As String = ""
-        For Each ch As Char In str1
-            Dim asciiValue As Integer = Asc(ch)
-            Dim binaryValue As String = Convert.ToString(asciiValue, 2).PadLeft(8, "0"c)
-            res &= binaryValue & " "
+
+    Function InitialPermutation(ByVal input8 As String) As String
+        Dim ipTable As Integer() = {8, 1, 2, 3, 4, 5, 6, 7}
+        Dim result As New System.Text.StringBuilder()
+        For i As Integer = 0 To 7
+            result.Append(input8(ipTable(i) - 1))
         Next
-        Return res
+        Return result.ToString()
     End Function
 End Class
